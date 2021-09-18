48452799927f5736753bbe9343af2ad182e11c5d
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-18 02:52:37 -0700

    c  client: scr: pages: Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
+import Cart from '../components/Cart';
 
 const Home = () => {
-  return (
-    <div className="container">
-      <CategoryMenu />
-      <ProductList />
-    </div>
-  );
+
+    return (
+        <div className="container">
+            <CategoryMenu />
+            <ProductList />
+            <Cart />
+        </div>
+    );
 };
 
 export default Home;
