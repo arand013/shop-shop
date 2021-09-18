48b82c6f307259f79fdf80da3c031822b721c0c5
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-09-18 04:04:03 -0700

    client: src: __tests__: reducers.test.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+import { Provider } from 'react-redux';
 
+import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from "./pages/OrderHistory";
+import Success from './pages/Success';
+
+import store from './utils/store';
+
 const client = new ApolloClient({
   request: (operation) => {
     const token = localStorage.getItem('id_token')
